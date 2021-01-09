540cb83b69b25332f30448a8f78ff4433d869f2f
Aydan Samedova <aydan2903@gmail.com>
2021-01-09 13:39:35 +0400

    FizzBuzz

---
+++ app.js
@@ -0,0 +1,18 @@
+let i = 1;
+let n = 101;
+
+function fizzBuzz() {
+    for (let i = 1; i < n; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            console.log("fizzbuzz")
+        } else if (i % 5 == 0) {
+            console.log("buzz")
+        } else if (i % 3 == 0) {
+            console.log("fizz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+fizzBuzz()
